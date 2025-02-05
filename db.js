359d9c9efbe508871c565f3b07b040f74561a722
hunterleeann <hunterleeann41210@gmail.com>
2025-02-04 19:09:22 -0800

    added user update abilities

--- db.js
+++ db.js
@@ -12,13 +12,61 @@ const createNewUser = async (email, firstName, lastName, password) => {
   return response;
 };
 
-const getUser = async (email) => {
+const getUser = async (id) => {
   const response = prisma.user.findFirstOrThrow({
     where: {
-      email,
+      id,
     },
+    select: {
+        firstName: true,
+        lastLame: true,
+        id: true,
+        email: true,
+      },
   });
   return response;
 };
 
-module.exports = { createNewUser, getUser };
+const deleteUser = async (email) => {
+    const response = await prisma.user.delete({
+      where: {
+        email,
+      }, 
+    });
+    return response;
+  };
+
+const getUsers = async () => {
+    const response = await prisma.user.findMany({});
+    return response;
+  };
+
+const getClient = async (id) => {
+    const response = await prisma.user.findFirstOrThrow({
+      where: {
+        id,
+      },
+    });
+    return response;
+  };
+
+  const updateUser = async ( id, email, firstName, lastName, password) => {
+    const response = await prisma.user.update({
+        where:{
+            id
+        },
+      data: {
+        email,
+        firstName,
+        lastName,
+        password,
+      },
+    });
+    return response;
+  };
+
+  //Create an update endpoint for a user. This should take in the email, first name, last name, and password, then update the user's information. Don’t forget to use bcrypt for the password. This is also a protected route
+
+
+
+module.exports = { createNewUser, getUser, getUsers, getClient, deleteUser, updateUser };
--- index.js
+++ index.js
@@ -7,10 +7,38 @@ const bcrypt = require("bcrypt");
 const JWT_SECRET = process.env.JWT_SECRET || "1234";
 const jwt = require("jsonwebtoken");
 
-const { createNewUser, getUser } = require("./db");
+const {
+  createNewUser,
+  getUser,
+  getUsers,
+  getClient,
+  deleteUser,
+  updateUser,
+} = require("./db");
 
 const setToken = (id) => {
   return jwt.sign({ id }, JWT_SECRET, { expiresIn: "24h" });
+};
+
+const isLoggedIn = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.slice(7);
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized: No token provided" });
+  }
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const customer = await getClient(id);
+    if (!customer) {
+      return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    }
+    req.customer = customer;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ error: "Unauthorized: Invalid or expired token" });
+  }
 };
 
 app.post("/register", async (req, res, next) => {
@@ -33,8 +61,8 @@ app.post("/register", async (req, res, next) => {
 
 app.post("/login", async (req, res, next) => {
   try {
-    const { firstName, lastEmail, email, password } = req.body;
-    const customer = await getUser(firstName, lastEmail, email);
+    const { email, password } = req.body;
+    const customer = await getUser(email);
     const match = await bcrypt.compare(password, customer.password);
     if (match) {
       const token = setToken(customer.id);
@@ -42,6 +70,65 @@ app.post("/login", async (req, res, next) => {
     } else {
       res.status(403).json({ message: "Username and Password do not match" });
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/aboutMe", isLoggedIn, async (req, res, next) => {
+  try {
+    //const customer = await getUser();
+    res.status(200).send(req.customer);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/users", isLoggedIn, async (req, res, next) => {
+  try {
+    const response = await getUsers();
+    res.status(200).send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/user", isLoggedIn, async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const response = await getUser(id);
+    res.status(200).send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/user", isLoggedIn, async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const response = await deleteUser(email);
+    res.status(200).send(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put("/update/:id", isLoggedIn, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { email, firstName, lastName, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const response = await updateUser(
+      id,
+      email,
+      firstName,
+      lastName,
+      hashPassword
+    );
+
+    res.status(200).send(response);
   } catch (error) {
     next(error);
   }
